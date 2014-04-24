28cc29b723c1559899c1546e7c56ca252a967c51
Jindra <rysavy.jindrich@seznam.cz>
2014-04-24 16:21:29 +0200

    test-commit-jindra

--- src/file/tree/analyzer/FileTreeAnalyzer.java
+++ src/file/tree/analyzer/FileTreeAnalyzer.java
@@ -14,6 +14,6 @@ public class FileTreeAnalyzer {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World2!");
+        System.out.println("Hello World3!");
     }
 }
