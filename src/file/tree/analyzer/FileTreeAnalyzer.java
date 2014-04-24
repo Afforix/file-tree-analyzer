9dbd11f5d0d6fa8ac28f95598287528722a1b07f
afforix <plch.matej@gmail.com>
2014-04-24 21:41:40 +0200

    experimental implementation of listing contents of the given directory

--- src/file/tree/analyzer/FileTreeAnalyzer.java
+++ src/file/tree/analyzer/FileTreeAnalyzer.java
@@ -14,6 +14,9 @@ public class FileTreeAnalyzer {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("All team members successfully cloned the repository.");
+        System.out.println("Prints contents of the given directory:");
+        DiscExplorer explorer = new DiscExplorer();
+        explorer.listFiles(".");
+        System.out.println("DONE");
     }
 }
