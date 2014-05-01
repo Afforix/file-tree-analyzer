232b438c6b07dc5611e694fc7006a40ace907958
suomi <suomiy@gmail.com>
2014-05-01 13:05:52 +0200

    gui

---
+++ src/file/tree/analyzer/Controller.java
@@ -0,0 +1,121 @@
+package file.tree.analyzer;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.DirectoryChooser;
+
+public class Controller {
+
+    @FXML
+    private Parent root;
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TreeView<File> treeView;
+
+    @FXML
+    private TableView<TestClassAttribute> tableView;
+
+    @FXML
+    private TableColumn<TestClassAttribute, String> attributeName;
+
+    @FXML
+    private TableColumn<TestClassAttribute, String> attribute;
+
+    private ObservableList<TestClassAttribute> testData;
+    
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        attributeName.setCellValueFactory(
+                new PropertyValueFactory<>("attributeName"));
+
+        attribute.setCellValueFactory(
+                new PropertyValueFactory<>("attribute")); 
+        
+        testData = FXCollections.observableArrayList(
+            new TestClassAttribute("Name", "test.txt"),
+            new TestClassAttribute("Type", "plain text document (text/plain)"),
+            new TestClassAttribute("Size", "100 bytes"),
+            new TestClassAttribute("Location", "/home")            
+        );
+        
+        tableView.setItems(testData); 
+    }
+
+    @FXML
+    void handleNewAnalysisAction(ActionEvent event) {
+
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        File selectedDirectory = directoryChooser.showDialog(root.getScene().getWindow());
+
+        if (selectedDirectory != null) {
+
+            System.out.println(selectedDirectory.getAbsolutePath());
+
+            listFilesDemo(selectedDirectory);
+        }
+
+    }
+
+    @FXML
+    void handleOptionsAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleQuitAction(ActionEvent event) {
+        System.exit(0);
+    }
+
+    private void listFilesDemo(File selectedDirectory) {
+
+        TreeItem<File> r = new TreeItem<>(selectedDirectory);
+
+        for (File n : selectedDirectory.listFiles()) {
+            r.getChildren().add(new TreeItem<>(n));
+        }
+
+        r.setExpanded(true);
+        treeView.setRoot(r);
+    }
+
+    public  class TestClassAttribute {
+        
+        private final SimpleStringProperty attributeName;
+        private final SimpleStringProperty attribute;
+        
+        public TestClassAttribute(String attributeName,String attribute) {
+            
+            this.attribute =  new SimpleStringProperty(attribute);
+            this. attributeName = new SimpleStringProperty(attributeName);
+        }
+        
+        public String getAttributeName()
+        {
+            return attributeName.get();
+        }       
+ 
+        public String getAttribute()
+        {
+            return attribute.get();
+        }       
+    }
+
+}
--- src/file/tree/analyzer/FileTreeAnalyzer.java
+++ src/file/tree/analyzer/FileTreeAnalyzer.java
@@ -4,19 +4,40 @@
  */
 package file.tree.analyzer;
 
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  *
  * @author ansy
  */
-public class FileTreeAnalyzer {
+public class FileTreeAnalyzer extends Application{
 
+      
+    @Override
+    public void start(Stage primaryStage) throws IOException {     
+   //    setUserAgentStylesheet(STYLESHEET_MODENA); //STYLESHEET_CASPIAN
+        Parent root = FXMLLoader.load(getClass().getResource("FileTreeAnalyzer.fxml"));      
+        Scene scene = new Scene(root);  
+       
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("File Tree Analyzer");
+        primaryStage.show();        
+         
+        System.out.println("Prints contents of the given directory:");
+        DiscExplorer explorer = new DiscExplorer();
+        explorer.listFiles(".");
+        System.out.println("DONE");                 
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Prints contents of the given directory:");
-        DiscExplorer explorer = new DiscExplorer();
-        explorer.listFiles(".");
-        System.out.println("DONE");
+         launch(args);     
     }
 }
