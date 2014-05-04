d57daf7a19c700eeb531b45c999b696287448010
xvitovs1 <409920@mail.muni.cz>
2014-05-04 10:33:53 +0200

    FileInfoConverter - first attempt

---
+++ src/file/tree/analyzer/FileInfoConverter.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package file.tree.analyzer;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+/**
+ *
+ * @author martina
+ */
+public class FileInfoConverter {
+
+    /**
+     * Takes object representing root of the directory tree and converts it to
+     * XML DOM.
+     *
+     * @param root root of the directory tree
+     * @return XML DOM
+     */
+    public Document fileInfoToDom(FileInfo root) {
+        Document doc = null;
+
+        try {
+            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+
+            doc = docBuilder.newDocument();
+            doc.setXmlVersion("1.0");
+
+            // root element
+            if (root.isDirectory()) {
+                Element rootElement = doc.createElement("directory");
+                rootElement.setAttribute("name", root.getName());
+                doc.appendChild(rootElement);
+                List<FileInfo> children = new ArrayList<>(root.getChildren());
+                for (FileInfo child : children) {
+                    childrenToDom(doc, rootElement, child);
+                }
+            } else {
+                Element rootElement = doc.createElement("file");
+                rootElement.setTextContent(root.getName());
+                doc.appendChild(rootElement);
+            }
+
+        } catch (ParserConfigurationException ex) {
+            Logger.getLogger(FileInfoConverter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return doc;
+    }
+
+    private void childrenToDom(Document doc, Element parent, FileInfo root) {
+        if (root.isDirectory()) {
+            Element currentElement = doc.createElement("directory");
+            currentElement.setAttribute("name", root.getName());
+            parent.appendChild(currentElement);
+            List<FileInfo> children = new ArrayList<>(root.getChildren());
+            for (FileInfo child : children) {
+                childrenToDom(doc, currentElement, child);
+            }
+        } else {
+            Element currentElement = doc.createElement("file");
+            currentElement.setTextContent(root.getName());
+            parent.appendChild(currentElement);
+        }
+    }
+}
