679c56cb2df1dc8ba0a53802770672ee994b71db
xvitovs1 <409920@mail.muni.cz>
2014-06-07 10:11:34 +0200

    tests - setUp and tearDown method

--- test/file/tree/analyzer/FileInfoConverterTest.java
+++ test/file/tree/analyzer/FileInfoConverterTest.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package file.tree.analyzer;
 
+import java.io.File;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -29,10 +29,39 @@ public class FileInfoConverterTest {
 
     @Before
     public void setUp() throws Exception {
+        File root = new File(".", "testDir");
+        if (root.mkdir()) {
+            File f1 = new File(root, "file1.txt");
+            f1.createNewFile();
+
+            File d = new File(root, "dir1");
+            if (d.mkdir()) {
+                File f2 = new File(d, "file2.txt");
+                f2.createNewFile();
+                File f3 = new File(d, "file3.txt");
+                f3.createNewFile();
+            }
+        }
     }
 
     @After
     public void tearDown() throws Exception {
+        File root = new File(".", "testDir");
+
+        File f = new File(root, "file1.txt");
+        f.delete();
+        
+        File d = new File(root, "dir1");
+        
+        File f2 = new File(d, "file2.txt");
+        f2.delete();
+        
+        File f3 = new File(d, "file3.txt");
+        f3.delete();
+        
+        d.delete();
+
+        root.delete();
     }
 
     /**
@@ -42,14 +71,14 @@ public class FileInfoConverterTest {
     public void testFileInfoToDom() {
         System.out.println("fileInfoToDom test");
         FileInfo root = createTestFileInfo();
-        
+
         Document expResult = createTestDocument();
-        
+
         Document result = FileInfoConverter.fileInfoToDom(root);
-        
+
         Element expRoot = expResult.getDocumentElement();
         Element resultRoot = result.getDocumentElement();
-        
+
         assertEquals(expRoot.getNodeName(), resultRoot.getNodeName());
         assertEquals(expRoot.getAttribute("name"), resultRoot.getAttribute("name"));
         assertEquals(expRoot.getAttribute("symbolicLink"), resultRoot.getAttribute("symbolicLink"));
@@ -60,25 +89,24 @@ public class FileInfoConverterTest {
         assertEquals(expRoot.getAttribute("creationTime"), resultRoot.getAttribute("creationTime"));
         assertEquals(expRoot.getAttribute("lastAccesTime"), resultRoot.getAttribute("lasAccessTime"));
         assertEquals(expRoot.getAttribute("lastModifiedTime"), resultRoot.getAttribute("lastModifiedTime"));
-        
+
     }
-    
+
     /**
      * Test of fileInfoToDom method with illegal argument.
      */
     @Test
     public void testNullFileInfoToDom() {
         System.out.println("fileInfoToDom with illegal argument test");
-        
+
         try {
             Document result = FileInfoConverter.fileInfoToDom(null);
             fail("didn't throw IllegalArgumentException for invalid input");
-        } catch(IllegalArgumentException ex) {
-            
-        }       
+        } catch (IllegalArgumentException ex) {
+
+        }
     }
-    
-    
+
     /**
      * Test of domToFileInfo method, of class FileInfoConverter.
      */
@@ -92,23 +120,22 @@ public class FileInfoConverterTest {
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
-    
-    
+
     /**
      * Test of domToFileInfo method with illegal argument.
      */
     @Test
     public void testNullDomToFileInfo() {
         System.out.println("domToFileInfo with illegal argument test");
-        
+
         try {
             FileInfo result = FileInfoConverter.domToFileInfo(null);
             fail("didn't throw IllegalArgumentException for invalid input");
-        } catch(IllegalArgumentException ex) {
-            
+        } catch (IllegalArgumentException ex) {
+
         }
     }
-    
+
     private FileInfo createTestFileInfo() {
         FileInfo root = new FileInfo();
         FileInfo child = new FileInfo();
@@ -120,12 +147,12 @@ public class FileInfoConverterTest {
             root.setName("testFolder");
             root.setNumberOfFiles(1);
             root.setNumberofDirectories(0);
-            
+
             DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss");
             root.setCreationTime(dateFormat.parse("2013-04-05T10:55:46"));
             root.setLastAccessTime(dateFormat.parse("2013-04-05T10:55:51"));
             root.setLastModifiedTime(dateFormat.parse("2013-04-05T10:55:46"));
-            
+
             child.setPath("/home/testFolder/testFile.txt");
             child.setAccessibility(true);
             child.setName("testFile.txt");
@@ -135,15 +162,15 @@ public class FileInfoConverterTest {
             child.setCreationTime(dateFormat.parse("2014-06-05T10:55:46"));
             child.setLastAccessTime(dateFormat.parse("2014-06-05T10:55:51"));
             child.setLastModifiedTime(dateFormat.parse("2014-06-05T10:55:46"));
-                        
+
             return root;
         } catch (ParseException ex) {
             //TODO
         }
-        
+
         return root;
     }
-    
+
     private Document createTestDocument() {
         Document doc = null;
         try {
@@ -176,14 +203,13 @@ public class FileInfoConverterTest {
             fileElement.setAttribute("creationTime", "2014-06-05T10:55:46");
             fileElement.setAttribute("lastAccessTime", "2014-06-05T10:55:51");
             fileElement.setAttribute("lastModifiedTime", "2014-06-05T10:55:46");
-            rootElement.appendChild(fileElement);     
-            
+            rootElement.appendChild(fileElement);
+
         } catch (ParserConfigurationException ex) {
             Logger.getLogger(FileInfoConverterTest.class.getName()).log(Level.SEVERE, null, ex);
         }
-           
+
         return doc;
     }
 
-    
 }
